c613a56eb930a3fec77abc94168bc838cdca437c
mollah111 <jalammollah111@gmail.com>
2024-10-06 18:22:00 +0600

    Variable Operator

---
+++ script.js
@@ -0,0 +1,48 @@
+
+console.log("Rahim has 300tk. He went to market and purchase 5 eggs, 2 kitkat chocolates. Price of each eggs 10tk, price of each chocolates 35tk. Find the remaining money.")
+
+const totalMoney = 300;
+
+var perEggPrice = 10;
+var eggBought = 5;
+
+let perChocolatePrice = 35;
+let chocolateBought = 2;
+
+var totalPrice = (eggBought * perEggPrice);
+
+let totalCost = (chocolateBought * perChocolatePrice);
+
+const total = (totalPrice + totalCost)
+
+const remainingMoney = (totalMoney - total)
+
+console.log(remainingMoney)
+
+
+// let rahimTotalTk = 300;
+
+// var perEggPrice = 10;
+// var eggBought = 5;
+
+// var perchocolatePrice = 35;
+// var chocolatebought = 2;
+
+// var totalPrice = (perEggPrice * eggBought) + (perchocolatePrice * chocolatebought)
+
+// let rahimRemaining = (rahimTotalTk - totalPrice)
+
+// console.log(rahimRemaining)
+
+
+// let fdonatMoney = 5000000;
+// let adddonatMoney = 2000000;
+
+// let totalMoney = (fdonatMoney + adddonatMoney)
+
+// const divideFamily = 90;
+
+// const eachFamily = (totalMoney / divideFamily)
+
+// console.log(eachFamily)
+
